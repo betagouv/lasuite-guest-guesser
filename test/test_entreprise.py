b15de11cb7dc50c0a11148409a41f01cc37c1683
Vincent Viers <vincent.viers@beta.gouv.fr>
2025-05-13 11:45:20 +0200

    add new tests

--- test/test_entreprise.py
+++ test/test_entreprise.py
@@ -27,18 +27,13 @@ def mock_lookup_locally(monkeypatch):
     Mock the _lookup_locally method of the Entreprise class.
     """
     return_value = {
-        "results": [
-            {
-                "nom_complet": "Test Company",
-                "complements": {
-                    "est_l100_3": True,
-                    "est_service_public": False,
-                    "collectivite_territoriale": None
-                }
-            }
-        ]
+        "nom": "Test Company",
+        "est_l100_3": True,
+        "est_service_public": True,
+        "nature_juridique": "1234",
+        "collectivite_territoriale": True
     }
-    monkeypatch.setattr('app.entreprise.Entreprise._lookup_via_api', lambda self: return_value)
+    monkeypatch.setattr('app.entreprise.Entreprise._lookup_locally', lambda self: return_value)
     
 
 def test_entreprise_lookup_success(mock_lookup_via_api):
@@ -50,7 +45,12 @@ def test_entreprise_lookup_success(mock_lookup_via_api):
     assert entreprise.service_public is False
     assert entreprise.collectivite_territoriale is False
 
-def test_entreprise_lookup_fallback(monkeypatch):
+def test_entreprise_lookup_fallback(monkeypatch, mock_lookup_locally):
     monkeypatch.setattr('app.entreprise.Entreprise._lookup_via_api', lambda self: ValueError("No results found"))
     entreprise = Entreprise("12345678901234")
     assert entreprise.siret == "12345678901234"
+    assert entreprise.siren == "123456789"
+    assert entreprise.nom == "Test Company"
+    assert entreprise.l100_3 is True
+    assert entreprise.service_public is True
+    assert entreprise.collectivite_territoriale is True
---
+++ test/test_entreprise_real_data.py
@@ -0,0 +1,31 @@
+import pytest
+
+from app.entreprise import Entreprise
+
+test_data = [
+    # SIRET, service_public, l100_3, collectivite_territoriale
+    ("21130055300016", True, True, True), # Ville de Marseille
+    ("13002526500013", True, True, False), # DINUM
+    ("059804062", True, False, False), # Régie des Transports Métropolitains
+    ("130000805", True, False, False), # GIP Prevention Protection Judiciaire Jeunesse
+    ("54205118000066", False, False, False), # TotalEnergies
+]
+
+@pytest.mark.parametrize("siret,service_public,l100_3,collectivite_territoriale", test_data)
+def test_with_real_siret(monkeypatch, siret, service_public, l100_3, collectivite_territoriale):
+    """
+    Test the Entreprise class with a real SIRET number. 
+    In theory, the results should be the same whether the API is used or the local CSV file.
+    """
+    # Replace with a valid SIRET number for testing
+    entreprise_api = Entreprise(siret)
+    assert entreprise_api.service_public is service_public
+    assert entreprise_api.l100_3 is l100_3
+    assert entreprise_api.collectivite_territoriale is collectivite_territoriale
+
+    # Turn off API access and test with local csv
+    monkeypatch.setattr('app.entreprise.Entreprise._lookup_via_api', lambda self: ValueError("No results found"))
+    entreprise_csv = Entreprise(siret)
+    assert entreprise_csv.service_public == entreprise_api.service_public
+    assert entreprise_csv.l100_3 == entreprise_api.l100_3
+    assert entreprise_csv.collectivite_territoriale == entreprise_api.collectivite_territoriale
